b14cd82e26d17dbae793addc3389219bce008ab2
Trond Klakken <trond@klakken.com>
2015-12-01 20:39:52 +0100

    Make it easier by installing an RDP + need sudo access to add to autostart

--- install_all.sh
+++ install_all.sh
@@ -1,7 +1,7 @@
 sudo su
 
 
-aptitude install xwit git nodejs npm x11-xserver-utils chromium screen ttf-mscorefonts-installer
+aptitude install xwit git nodejs npm x11-xserver-utils chromium screen ttf-mscorefonts-installer xrdp
 
 # xwit => Controll mouse cursor
 # Git => code version control
@@ -9,6 +9,9 @@ aptitude install xwit git nodejs npm x11-xserver-utils chromium screen ttf-mscor
 # x11-xserver-utils => xset to disable screensaver and battery sleep
 # chromium => Browser to display whatever
 # screen => Handy tool to run long lasting tasks when using ssh
+
+# Fix chromium error:
+sudo ln -s /usr/lib/arm-linux-gnueabihf/nss/ /usr/lib/nss
 
 cd /home/pi
 
@@ -22,4 +25,5 @@ cd /home/pi/info-pi-client/
 chomod a+x *.sh
 
 # Legg inn sånn at startup kjører og!
-'/bin/bash /home/pi/info-pi-client/startup.sh' >> /etc/xdg/lxsession/LXDE/autostart
+sudo su
+echo '/bin/bash /home/pi/info-pi-client/startup.sh' >> /etc/xdg/lxsession/LXDE/autostart
