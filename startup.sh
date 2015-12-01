a01aca6e9afd93d1e2a4227c9f28a51a960295aa
Trond Klakken <trond@klakken.com>
2015-12-01 21:02:25 +0100

    Check that startup isn't run twice

--- startup.sh
+++ startup.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-# Found this tutorial after the fact, looks good: https://www.danpurdy.co.uk/web-development/raspberry-pi-kiosk-screen-tutorial/
+CHECK=`ps aux | grep "python -m SimpleHTTPServer 8080" | wc -l`
 
-export DISPLAY=:0
+if [ $CHECK -lt 2]; then
 
-# Turn off screen sleep
-xset s off
-xset -dpms
-xset s noblank
+	# Found this tutorial after the fact, looks good: https://www.danpurdy.co.uk/web-development/raspberry-pi-kiosk-screen-tutorial/
 
-# start python server
-cd /home/pi/infoskjerm
-python -m SimpleHTTPServer 8080 &
+	export DISPLAY=:0
 
-./home/pi/info-pi-client/move_mouse_cursor_to_corner.sh
+	# Turn off screen sleep
+	xset s off
+	xset -dpms
+	xset s noblank
 
-# Start chromium again if it dies
-while true
-do
-	sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' /home/pi/.config/chromium/Default/Preferences
-	# Show chromium in kiosk mode (fullscreen)
-	chromium --noerrdialogs --kiosk http://localhost:8080
-done
+	# start python server
+	cd /home/pi/infoskjerm
+	python -m SimpleHTTPServer 8080 &
+
+	cd /home/pi/info-pi-client/
+	./move_mouse_cursor_to_corner.sh
+
+	# Start chromium again if it dies
+	#while true
+	#do
+		sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' /home/pi/.config/chromium/Default/Preferences
+		# Show chromium in kiosk mode (fullscreen)
+		chromium --noerrdialogs --kiosk http://localhost:8080
+
+		killall chromium
+	#done
+
+fi
